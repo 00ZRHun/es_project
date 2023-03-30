22e8244b8dff1f653f8de06094e44da80a94a53c
00ZRHun <00zrhun@gmail.com>
2023-03-30 15:16:49 +0800

    feat: makemigrations & migrate

--- es_project/settings.py
+++ es_project/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Tweets application
+    'tweets.apps.TweetsConfig',
     # Django REST framework
     'rest_framework',
 ]
--- tweets/models.py
+++ tweets/models.py
@@ -2,4 +2,5 @@ from django.db import models
 
 
 class Tweet(models.Model):
+    term = models.CharField(max_length=70, blank=False, default='')
     data = models.JSONField
